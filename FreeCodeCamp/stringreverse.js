4e4337c84bda50a424575a9a51dbda999ae6ab84
Joel Hart <joel.d.hart1@gmail.com>
2023-01-04 14:24:57 -0800

    added reversestring algorithm

--- FreeCodeCamp/stringreverse.js
+++ FreeCodeCamp/stringreverse.js
@@ -25,3 +25,23 @@ function reverseString(str) {
   // Step 3. Return the reversed string
   return newString; // "olleh
 }
+
+// Using split method to make an array, and then reversing using reverse array method and then join method.
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+}
+
+reverseString("hello");
